59014158814b5c2f1caebf950ec331b9cfeb5725
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-10-22 06:17:30 +0000

    Update siteProps in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sohail Ahamed",
+  title: "Web Developer & UI/UX Designer",
+  email: "sohaildotcss@gmail.com",
+  gitHub: "sohaildotcss",
+  instagram: "sohail.css",
+  linkedIn: "sohaildotcss",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sohaildotcss",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
